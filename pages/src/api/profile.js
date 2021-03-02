625670b67c47dd6ba6f982181b63f00f38755882
kimikimi <1543393961@qq.com>
2021-03-02 16:43:51 +0800

    前端页面

---
+++ pages/src/api/profile.js
@@ -0,0 +1,39 @@
+import request from '@/utils/request'
+
+/**
+ * 查看个人信息
+ * @param username
+ * @returns {AxiosPromise}
+ */
+export function info(username) {
+  return request({
+    url: '/profile/info/'+username,
+    method: 'get'
+  })
+}
+
+/**
+ * 更新个人信息
+ * @param data
+ * @returns {AxiosPromise}
+ */
+export function update(data) {
+  return request({
+    url: '/profile/update/',
+    method: 'post',
+    data
+  })
+}
+
+/**
+ * 更新头像
+ * @param data
+ * @returns {AxiosPromise}
+ */
+export function modifyIcon(data) {
+  return request({
+    url: '/profile/modify/icon',
+    method: 'post',
+    data
+  })
+}
---
+++ pages/src/store/modules/user.js
@@ -0,0 +1,113 @@
+import { login, logout, getInfo } from '@/api/user'
+import { getToken, setToken, removeToken } from '@/utils/auth'
+import { resetRouter } from '@/router'
+
+const getDefaultState = () => {
+  return {
+    token: getToken(),
+    name: '',
+    avatar: ''
+  }
+}
+
+const state = getDefaultState()
+
+/**
+ * this.$store.commit('changeValue',name) 同步操作
+ * @type {{SET_TOKEN: mutations.SET_TOKEN, SET_AVATAR: mutations.SET_AVATAR, SET_NAME: mutations.SET_NAME, RESET_STATE: mutations.RESET_STATE}}
+ */
+const mutations = {
+  RESET_STATE: (state) => {
+    Object.assign(state, getDefaultState())
+  },
+  SET_TOKEN: (state, token) => {
+    state.token = token
+  },
+  SET_NAME: (state, name) => {
+    state.name = name
+  },
+  SET_AVATAR: (state, avatar) => {
+    state.avatar = avatar
+  }
+}
+
+/**
+ * this.$store.dispatch('
+ * @type {{logout({commit?: *, state: *}): Promise<unknown>, getInfo({commit?: *, state: *}): Promise<unknown>, resetAvatar({commit: *}, *=): Promise<unknown>, login({commit?: *}, *): Promise<unknown>, resetToken({commit: *}): Promise<unknown>}}
+ */
+const actions = {
+  // user login
+  login({ commit }, userInfo) {
+    const { username, password } = userInfo
+    return new Promise((resolve, reject) => {
+      login({ username: username.trim(), password: password }).then(response => {
+        const { data } = response
+        commit('SET_TOKEN', data.token)
+        setToken(data.token)
+        resolve()
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  },
+
+  // get user info
+  getInfo({ commit, state }) {
+    return new Promise((resolve, reject) => {
+      getInfo(state.token).then(response => {
+        const { data } = response
+
+        if (!data) {
+          return reject('Verification failed, please Login again.')
+        }
+
+        const { name, avatar } = data
+
+        commit('SET_NAME', name)
+        commit('SET_AVATAR', avatar)
+        resolve(data)
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  },
+
+  // user logout
+  logout({ commit, state }) {
+    return new Promise((resolve, reject) => {
+      logout(state.token).then(() => {
+        removeToken() // must remove  token  first
+        resetRouter()
+        commit('RESET_STATE')
+        resolve()
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  },
+
+  // remove token
+  resetToken({ commit }) {
+    return new Promise(resolve => {
+      removeToken() // must remove  token  first
+      commit('RESET_STATE')
+      resolve()
+    })
+  },
+
+  // 设置头像
+  resetAvatar({ commit },avatar) {
+    return new Promise(resolve => {
+      commit('SET_AVATAR',avatar)
+      resolve()
+    })
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
+
